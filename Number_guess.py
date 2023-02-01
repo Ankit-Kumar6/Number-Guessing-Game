71c724fba3a90f043fe710733dc60e1f7402e009
Ankit-Kumar6 <57087618+Ankit-Kumar6@users.noreply.github.com>
2023-02-01 11:41:55 +0530

    Update Number_guess.py

--- Number_guess.py
+++ Number_guess.py
@@ -1,4 +1,4 @@
-#This is a Guess Number game.
+#This is a Number Guessing game.
 import random
 
 guesses_taken = 0
@@ -9,6 +9,7 @@ my_name = input()
 number = random.randint(1, 20)
 print(f"Well, {my_name}, I am thinking of a number between 1 and 20")
 
+#This was for the hint, when you divide a number by 2 if the reminder is 1 then it's an odd number. If the remainder is 0 then it's an even number.
 odd_number = number%2
 even_number = number%2
 
@@ -19,7 +20,7 @@ for guesses_taken in range(7):
 
     if guess < number:
         diff = number - guess
-        if diff in range (3):
+        if diff in range (3):              #Here I added the diff var, so if the number is 5 and guess is 4, then the o/p will not throw "your guess is too low".
             print("Your guess is low!")
         else:
             print("Your Guess is too low!")
@@ -31,15 +32,15 @@ for guesses_taken in range(7):
             print("Your guess is too high!")
     if guess == number:
         break
-    if guesses_taken == 4:
+    if guesses_taken == 4:     #Added a hint on the 4th guess.
         print("Here's a hint for you.")
-        if odd_number == 1:
-            print("The number is an odd number.")
+        if odd_number == 1:         
+            print("The number is an odd number.")     
         elif even_number == 0:
             print("The number is an even number.")
 
 if guess == number:
-    guesses_taken = guesses_taken + 1
+    guesses_taken = guesses_taken + 1         #Because Guesses-taken startes from 0.
     print(f"Good job,{my_name}! you guessed it in {guesses_taken} guesses!")
 
 if guess != number:
